03007bb3bd2aef719423172da2d9f7634ceeb893
= <=>
2017-12-02 20:44:48 -0200

    otimizacoes rs

--- prim.py
+++ prim.py
@@ -13,7 +13,7 @@ def mst_prim(G, r):
     # λ(r) = 0
     # Q = V
     #
-    # while Q ≠ 0 {
+    # while Q > 0 {
     #   u = ExtractMin(Q)
     #   for each v ∈ N(u) {
     #       if v ∈ Q and λ(v) > w(u, v) {
@@ -26,23 +26,19 @@ def mst_prim(G, r):
     # fila de prioridades e visitados
     queue, visitados = [], []
 
-    # for each v ∈ V {
-    for v in G.nodes():
 
-        # λ(v) = ∞
-        G.node[v]['lambda'] = float("inf")
-
-        # π(v) = NIL
-        G.node[v]['pi'] = None
+    # for each v ∈ V { λ(v) = ∞ ; π(v) = NIL }
+    lambdas = [float("inf")] * G.number_of_nodes()
+    pi = [None] * G.number_of_nodes()
 
     # λ(r) = 0
-    G.node[r]['lambda'] = 0
+    lambdas[r] = 0
 
     # Q = V
     for v in G.nodes():
-        queue.append((G.node[v]['lambda'], v))
+        queue.append((lambdas[v], v))
 
-    # while Q ≠ 0
+    # while Q > 0
     while len(queue) > 0:
 
         # u = ExtractMin(Q) ///// fila ordenada pelo valor do lambda para extrair o min
@@ -56,15 +52,15 @@ def mst_prim(G, r):
         for v in G.neighbors(u):
 
             # if v ∈ Q and λ(v) > w(u, v) /// se v nao tiver sido visitado e a se lambda de v for maior que distancia de u e v
-            if not v in visitados and G.node[v]['lambda'] > G[u][v]['weight']:
+            if not v in visitados and lambdas[v] > G[u][v]['weight']:
 
                 # λ(v) = w(u, v) /// remove a tupla <lambda, v> da fila de prioridades, atualiza seu valor, e depois recoloca a mesma na fila
-                queue.remove((G.node[v]['lambda'], v))
-                G.node[v]['lambda'] = G[u][v]['weight']
-                queue.append((G.node[v]['lambda'], v))
+                queue.remove((lambdas[v], v))
+                lambdas[v] = G[u][v]['weight']
+                queue.append((lambdas[v], v))
 
                 # π(v) = u
-                G.node[v]['pi'] = u
+                pi[v] = u
 
     # criacao da mst, na qual iremos colocar os valores resultantes do algoritmo
     mst = nx.Graph()
@@ -74,13 +70,13 @@ def mst_prim(G, r):
         mst.add_node(u)
 
         # definimos o predecessor para cada vertice u pertencente ao nosso grafo G
-        if G.node[u]['pi'] is not None:
+        if pi[u] is not None:
 
             # colocamos à aresta de seu predecessor
-            mst.add_edge(u, G.node[u]['pi'])
+            mst.add_edge(u, pi[u])
 
             # colocamos o peso (distancia) do nosso precedessor
-            mst[u][G.node[u]['pi']]['weight'] = G[u][G.node[u]['pi']]['weight']
+            mst[u][pi[u]]['weight'] = G[u][pi[u]]['weight']
 
     # retornamos mst
     return mst
